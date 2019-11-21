25f2ddf8625598b0c608b174640c36c29c0869ab
Arnaud <arnaudlemoine.69@gmail.com>
2019-11-21 11:15:53 +0100

    Update app.R
    Ajout de la librairie DT,
    Affichage des data grace à la librairie

--- Test1/app.R
+++ Test1/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(DT)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -30,21 +31,24 @@ ui <- fluidPage(
            plotOutput("distPlot")
         )
     ),
+    DT::dataTableOutput("carsPlot"),
     actionButton(inputId = "goButton", label = "test")
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    cars <- data(mtcars)
     
+    output$carsPlot <-  DT::renderDataTable({
+        mtcars
+    })
+        
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+        hist(x, breaks = bins, col = 'blue', border = 'white')
     })
     
 }
